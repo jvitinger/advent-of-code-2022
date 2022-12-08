0707c99eaec48b912731830457d60df880ace4e6
Jiri Vitinger <jiri.vitinger@avast.com>
2022-12-08 22:12:31 +0100

    8 - cosmetic changes

--- src/main/kotlin/08.kt
+++ src/main/kotlin/08.kt
@@ -1,51 +1,25 @@
 import java.io.FileReader
 
+class Tree(val height: Int, var visible: Boolean = false, var scenicScore: Int = 1)
+
 fun main(args: Array<String>) {
     val input = FileReader("input08.txt").readLines()
 
     var forest = buildForest(input)
 
     repeat(4) {
-        println()
-        println("================================ $it ================================")
         markVisibleFromLeft(forest)
         forest = rotate90(forest)
     }
 
-    println()
     println("Visible trees: ${forest.sumOf { row -> row.count { tree -> tree.visible } }}")
 
     repeat(4) {
-        println()
-        println("================================ $it ================================")
         calculateScenicScoreToLeft(forest)
         forest = rotate90(forest)
     }
 
-    println()
     println("Max scenic score: ${forest.maxOf { row -> row.maxOf { tree -> tree.scenicScore } }}")
-}
-
-private fun calculateScenicScoreToLeft(forest: MutableList<MutableList<Tree>>) {
-    forest.indices.forEach { i ->
-        println()
-        forest[i].indices.forEach { j ->
-            forest[i][j].scenicScore *= calculateScenicScoreToLeft(forest, i, j)
-            print("  ${forest[i][j].scenicScore}  ")
-        }
-    }
-}
-
-fun calculateScenicScoreToLeft(forest: MutableList<MutableList<Tree>>, i: Int, j: Int): Int {
-    val baseHeight = forest[i][j].height
-    var scenicScore = 0
-    (j + 1 until forest[i].size).forEach {
-        scenicScore++
-        if (forest[i][it].height >= baseHeight) {
-            return scenicScore
-        }
-    }
-    return scenicScore
 }
 
 private fun buildForest(input: List<String>): MutableList<MutableList<Tree>> {
@@ -67,9 +41,6 @@ private fun markVisibleFromLeft(forest: MutableList<MutableList<Tree>>) {
             if (tree.height > maxHeight) {
                 maxHeight = tree.height
                 tree.visible = true
-                print(tree.height)
-            } else {
-                print(" ")
             }
         }
     }
@@ -87,10 +58,23 @@ fun rotate90(forest: MutableList<MutableList<Tree>>): MutableList<MutableList<Tr
     return transposed
 }
 
-class Tree(
-    val height: Int,
-    var visible: Boolean = false,
-    var scenicScore: Int = 1
-)
+private fun calculateScenicScoreToLeft(forest: MutableList<MutableList<Tree>>) {
+    forest.indices.forEach { i ->
+        println()
+        forest[i].indices.forEach { j ->
+            forest[i][j].scenicScore *= calculateScenicScoreToLeft(forest, i, j)
+        }
+    }
+}
 
-
+private fun calculateScenicScoreToLeft(forest: MutableList<MutableList<Tree>>, i: Int, j: Int): Int {
+    val baseHeight = forest[i][j].height
+    var scenicScore = 0
+    (j + 1 until forest[i].size).forEach {
+        scenicScore++
+        if (forest[i][it].height >= baseHeight) {
+            return scenicScore
+        }
+    }
+    return scenicScore
+}
